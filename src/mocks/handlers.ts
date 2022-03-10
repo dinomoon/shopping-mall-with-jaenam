2613c672758d3a701d055c0962bff6385cac36ab
dinomoon <ansrud45@gmail.com>
2022-03-10 17:56:43 +0900

    3일차

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,19 +1,24 @@
-// src/mocks/handlers.js
 import { graphql } from 'msw';
-import { v4 as uuid } from 'uuid';
-import { ADD_CART, Cart, GET_CART } from '../graphql/cart';
-import { GET_PRODUCT, GET_PRODUCTS } from '../graphql/products';
+import {
+  GET_CART,
+  ADD_CART,
+  CartType,
+  UPDATE_CART,
+  DELETE_CART,
+} from '../graphql/cart';
+import { GET_PRODUCTS, GET_PRODUCT } from '../graphql/products';
 
-const mockProducts = Array.from({ length: 20 }).map((_, i) => ({
-  id: `${i + 1}`,
-  imageUrl: `https://placeimg.com/200/150/${i + 1}`,
-  price: 50000,
-  title: `임시상품${i + 1}`,
-  description: `임시상세내용${i + 1}`,
-  createdAt: new Date(1645735501883 + i * 1000 * 60 * 60 * 24).toString(),
-}));
+const mockProducts = (() =>
+  Array.from({ length: 20 }).map((_, i) => ({
+    id: i + 1 + '',
+    imageUrl: `https://placeimg.com/200/150/${i + 1}`,
+    price: 50000,
+    title: `임시상품${i + 1}`,
+    description: `임시상세내용${i + 1}`,
+    createdAt: new Date(1645735501883 + i * 1000 * 60 * 60 * 10).toString(),
+  })))();
 
-let cartData: { [key: string]: Cart } = {};
+let cartData: { [key: string]: CartType } = {};
 
 export const handlers = [
   graphql.query(GET_PRODUCTS, (req, res, ctx) => {
@@ -26,8 +31,8 @@ export const handlers = [
 
   graphql.query(GET_PRODUCT, (req, res, ctx) => {
     const found = mockProducts.find((item) => item.id === req.variables.id);
-    if (!found) return res();
-    return res(ctx.data(found));
+    if (found) return res(ctx.data(found));
+    return res();
   }),
 
   graphql.query(GET_CART, (req, res, ctx) => {
@@ -35,24 +40,47 @@ export const handlers = [
   }),
 
   graphql.mutation(ADD_CART, (req, res, ctx) => {
-    const newData = { ...cartData };
+    const newCartData = { ...cartData };
     const id = req.variables.id;
-    if (newData[id]) {
-      newData[id] = {
-        ...newData[id],
-        amount: newData[id].amount + 1,
-      };
-    } else {
-      const found = mockProducts.find((item) => item.id === req.variables.id);
-      if (found) {
-        newData[id] = {
-          ...found,
-          amount: 1,
-        };
-      }
+    const targetProduct = mockProducts.find(
+      (item) => item.id === req.variables.id
+    );
+
+    if (!targetProduct) {
+      throw new Error('상품이 없습니다');
     }
 
+    const newItem = {
+      ...targetProduct,
+      amount: (newCartData[id]?.amount || 0) + 1,
+    };
+    newCartData[id] = newItem;
+    cartData = newCartData;
+
+    return res(ctx.data(newItem));
+  }),
+
+  graphql.mutation(UPDATE_CART, (req, res, ctx) => {
+    const newData = { ...cartData };
+    const { id, amount } = req.variables;
+    if (!newData[id]) {
+      throw new Error('없는 데이터입니다');
+    }
+    const newItem = {
+      ...newData[id],
+      amount,
+    };
+    newData[id] = newItem;
     cartData = newData;
-    return res(ctx.data(newData));
+
+    return res(ctx.data(newItem));
+  }),
+
+  graphql.mutation(DELETE_CART, ({ variables: { id } }, res, ctx) => {
+    const newData = { ...cartData };
+    delete newData[id];
+    cartData = newData;
+
+    return res(ctx.data(id));
   }),
 ];
--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -4,16 +4,15 @@ import { CartType, GET_CART } from '../../graphql/cart';
 import { graphqlFetcher, QueryKeys } from '../../queryClient';
 
 const Cart = () => {
-  const { data } = useQuery(QueryKeys.CART, () => graphqlFetcher(GET_CART));
+  const { data } = useQuery(QueryKeys.CART, () => graphqlFetcher(GET_CART), {
+    staleTime: 0,
+    cacheTime: 0,
+  });
   const cartItems = Object.values(data || {}) as CartType[];
-
-  console.log(data);
 
   if (cartItems.length === 0) {
     return <div>장바구니가 비었어요.</div>;
   }
-
-  console.log(cartItems);
 
   return <CartList items={cartItems} />;
 };
